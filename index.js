a73da33128e2c457019da146967a65c4b109c152
Vithusha Ravirajan <vithusharavirajan@Vithus-MBP.local>
2023-09-10 22:24:35 -0400

    complete

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
+async function fetchWeatherData(apikey, path) {
+  try {
+      const res = await fetch(path);
+      if (!res.ok) {
+          throw new Error(`HTTP error! Status: ${res.status}`);
+      }
+      const data = await res.json();
 
-      // Functions 
-function getWeather(apikey, zip, tempEl, descEl) {
-      // Replace this with your own API key!
-      const apiKey = apikey // '467355df4c808dd6134a3b64e9ace282'
-      const units = 'imperial'
-      const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
-      fetch(path)
-        .then(res => res.json())
-        .then(json => {
-          tempEl.innerHTML = json.main.temp
-          descEl.innerHTML = json.weather[0].description
-        })
-        .catch(err => console.log(err.message))
-    }
-         
+      return {
+          temperature: data.main.temp,
+          description: data.weather[0].description,
+          humidity: data.main.humidity,
+          windSpeed: data.wind.speed,
+          cityName: data.name,
+          icon: data.weather[0].icon,
+          country: data.sys.country
+      };
+  } catch (err) {
+      console.error(err.message);
+      return null;
+  }
+}
 
-export default getWeather;
+async function getWeatherByZip(apikey, zip, unit = 'imperial') {
+  const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apikey}&units=${unit}`;
+  return await fetchWeatherData(apikey, path);
+}
+
+async function getWeatherByCity(apikey, city, unit = 'imperial') {
+  const path = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=${unit}`;
+  return await fetchWeatherData(apikey, path);
+}
+
+async function getWeatherByGeo(apikey, lat, lon, unit = 'imperial') {
+  const path = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}&units=${unit}`;
+  return await fetchWeatherData(apikey, path);
+}
+
+export { getWeatherByZip, getWeatherByCity, getWeatherByGeo };
